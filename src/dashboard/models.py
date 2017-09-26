d2187949cc114c8711bacb61e6c0de343e67c5b2
Hillux <gachukihillary@gmail.com>
2017-09-26 22:35:26 +0300

    fixed admin login in models

--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -8,7 +8,7 @@ class Bio_data(models.Model):
 	sir_name = models.CharField(null=False,blank=False,max_length=30,default='')
 	first_name = models.CharField(null=False,blank=False,max_length=30,default='')
 	second_name = models.CharField(null=False,blank=False,max_length=30,default='')
-	DOB = models.DateTimeField(null=True,blank=True)
+	DOB = models.DateField(null=True,blank=True)
 	phone_no = models.IntegerField(null=False,blank=False)
 	id_no = models.IntegerField(null=False,blank=False)
 	age = models.IntegerField(null=True,blank=True)
@@ -95,7 +95,7 @@ class Clinician_login(models.Model):
 	job_number = models.IntegerField(null=False,blank=False)
 	last_login_time = models.DateTimeField(auto_now = True)
 	def __str__(self):
-		return email_address
+		return self.email_address
 	class Admin:
 		pass
 	class Meta:
@@ -108,7 +108,7 @@ class Admin_login(models.Model):
 	Login_time = models.DateTimeField(auto_now_add = False,auto_now = True)
 	pswd = models.CharField(max_length=30,null=False,blank=False)
 	"""docstring for Admin_Login"""
-	def __init__(self):
+	def __str__(self):
 		return self.email_address
 		
 
